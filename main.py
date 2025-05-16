0b3d6d30097926fe6f499daff8874c4d84f12496
HAKPIN LEE <hakbin9984@gmail.com>
2025-05-16 21:47:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from fastapi import FastAPI
+from routers.test import router as test
 import uvicorn
-from fastapi import FastAPI, HTTPException
-from dbmodule import dbmodule
-import pandas as pd
-import datetime
-db = dbmodule()
+
 app = FastAPI()
+
+from routers.test import router as test
+
+
+app.include_router(test)
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
